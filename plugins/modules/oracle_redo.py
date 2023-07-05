4fa2e4daeb1d1cc221b0f7e8e7cc0cae5a794c3d
Thorsten Bruhns <thorsten.bruhns@googlemail.com>
2023-07-05 05:31:39 +0000

    oracle_redo: refactoring code, make it usable for ansible-doc, Python3 usable only

--- plugins/modules/oracle_redo.py
+++ plugins/modules/oracle_redo.py
@@ -1,10 +1,12 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+from ansible.module_utils.basic import AnsibleModule
 
 ANSIBLE_METADATA = {
     'metadata_version': '1.1',
     'status': ['preview'],
-    'supported_by': 'community'
+    'supported_by': 'community',
 }
 
 DOCUMENTATION = '''
@@ -17,48 +19,43 @@ description:
 version_added: "2.4"
 options:
     hostname:
-        description:
-            - The Oracle database host
+        description: The Oracle database host
         required: false
         default: localhost
     port:
-        description:
-            - The listener port number on the host
+        description: >
+            The listener port number on the host
         required: false
         default: 1521
     service_name:
-        description:
-            - The database service name to connect to
+        description: >
+            The database service name to connect to
         required: true
     user:
-        description:
-            - The Oracle user name to connect to the database, must have DBA privilege
+        description: >
+            The Oracle user name to connect to the database, must have DBA privilege
         required: False
     password:
-        description:
-            - The Oracle user password for 'user'
+        description: The Oracle user password for 'user'
         required: False
     mode:
-        description:
-            - The mode with which to connect to the database
+        description: >
+            The mode with which to connect to the database
         required: true
         default: normal
         choices: ['normal','sysdba']
     size:
-        description:
-            - size of redologs
-        required:
-            - True
-        default:
-            - 50MB
+        description: size of redologs
+        required: true
+        default: 50MB
     groups:
-        description:
-            - The number of redolog groups
-        required:
-            - False
+        description: The number of redolog groups
+        required: false
+        type: int
     members:
-        description:
-            - Either to set the preference (present) or reset it to default (absent)
+        description: >
+            Either to set the preference (present)
+            or reset it to default (absent)
         required: true
         default: 1
 
@@ -81,7 +78,6 @@ EXAMPLES = '''
       oracle_env:
              ORACLE_HOME: "{{ oracle_home }}"
              LD_LIBRARY_PATH: "{{ oracle_home }}/lib"
-
       redosize: 15M
       numgroups: 3
   tasks:
@@ -104,25 +100,24 @@ except ImportError:
 else:
     cx_oracle_exists = True
 
+
 # Ansible code
 def main():
     global lconn, conn, msg, module
     msg = ['']
     module = AnsibleModule(
-        argument_spec = dict(
-            hostname      = dict(default='localhost'),
-            port          = dict(default=1521, type='int'),
-            service_name  = dict(required=True),
-            user          = dict(required=False),
-            password      = dict(required=False, no_log=True),
-            mode          = dict(default='normal', choices=["normal","sysdba"]),
-            size          = dict(required=True),
-            groups        = dict(required=True)
-        #     threads       = dict(default=1)
+        argument_spec=dict(
+            hostname=dict(default='localhost'),
+            port=dict(default=1521, type='int'),
+            service_name=dict(required=True),
+            user=dict(required=False),
+            password=dict(required=False, no_log=True),
+            mode=dict(default='normal', choices=["normal", "sysdba"]),
+            size=dict(required=True),
+            groups=dict(required=True)
+            #     threads       = dict(default=1)
         ),
-
     )
-
 
     hostname = module.params["hostname"]
     port = module.params["port"]
@@ -136,12 +131,20 @@ def main():
 
     # Check for required modules
     if not cx_oracle_exists:
-        module.fail_json(msg="The cx_Oracle module is required. 'pip install cx_Oracle' should do the trick. If cx_Oracle is installed, make sure ORACLE_HOME & LD_LIBRARY_PATH is set")
-
+        module.fail_json(
+            msg=(
+                "The cx_Oracle module is required. "
+                "'pip install cx_Oracle' should do the trick. "
+                "If cx_Oracle is installed, make sure ORACLE_HOME "
+                "& LD_LIBRARY_PATH is set"
+            )
+        )
 
     wallet_connect = '/@%s' % service_name
     try:
-        if (not user and not password ): # If neither user or password is supplied, the use of an oracle wallet is assumed
+        if not user and not password:
+            # If neither user or password is supplied, the use of an
+            # oracle wallet is assumed
             if mode == 'sysdba':
                 connect = wallet_connect
                 conn = cx_Oracle.connect(wallet_connect, mode=cx_Oracle.SYSDBA)
@@ -149,22 +152,29 @@ def main():
                 connect = wallet_connect
                 conn = cx_Oracle.connect(wallet_connect)
 
-        elif (user and password ):
+        elif user and password:
             if mode == 'sysdba':
-                dsn = cx_Oracle.makedsn(host=hostname, port=port, service_name=service_name)
+                dsn = cx_Oracle.makedsn(
+                    host=hostname, port=port, service_name=service_name
+                )
                 connect = dsn
                 conn = cx_Oracle.connect(user, password, dsn, mode=cx_Oracle.SYSDBA)
             else:
-                dsn = cx_Oracle.makedsn(host=hostname, port=port, service_name=service_name)
+                dsn = cx_Oracle.makedsn(
+                    host=hostname, port=port, service_name=service_name
+                )
                 connect = dsn
                 conn = cx_Oracle.connect(user, password, dsn)
 
-        elif (not(user) or not(password)):
+        elif not (user) or not (password):
             module.fail_json(msg='Missing username or password for cx_Oracle')
 
     except cx_Oracle.DatabaseError as exc:
-        error, = exc.args
-        msg[0] = 'Could not connect to database - %s, connect descriptor: %s' % (error.message, connect)
+        (error,) = exc.args
+        msg[0] = 'Could not connect to database - %s, connect descriptor: %s' % (
+            error.message,
+            connect,
+        )
         module.fail_json(msg=msg[0], changed=False)
     #
     # if module.check_mode:
@@ -172,7 +182,11 @@ def main():
     #
 
     if not (size.endswith('M') or size.endswith('G') or size.endswith('T')):
-        msg = 'You need to suffix the size with (M,G or T), i.e: %sM/%sG/%sT' % (size,size,size)
+        msg = 'You need to suffix the size with (M,G or T), i.e: %sM/%sG/%sT' % (
+            size,
+            size,
+            size,
+        )
         module.fail_json(msg=msg, changed=False)
 
     redosql = """
@@ -232,26 +246,36 @@ def main():
 
            -- Strip the suffix (M/G/T) from the input string
            IF upper(v_maxbytes_suffix) in ('M','G','T') THEN
-               select substr (v_maxbytes, 0, (length (v_maxbytes)-1)) into v_maxbytes_actual from dual;
+               select substr (v_maxbytes, 0, (length (v_maxbytes)-1))
+               into v_maxbytes_actual
+               from dual;
            END IF;
 
 
-               FOR rec in (select thread#, group#, status, bytes from v$log order by thread#)
+               FOR rec in (select thread#, group#, status, bytes
+                           from v$log
+                           order by thread#)
                LOOP
                    IF rec.bytes/v_divisor != v_maxbytes_actual    THEN
                        v_redo_size_changes := v_redo_size_changes+1;
-                       for chloop in (select thread#, group#, status from v$log where thread# = rec.thread# and group# = rec.group#)
+                       for chloop in (select thread#, group#, status
+                       from v$log
+                       where thread# = rec.thread# and group# = rec.group#)
                        loop
                            IF chloop.status in ('CURRENT','ACTIVE') THEN
                                  --dbms_output.put_line('Current');
                                  execute immediate v_sql_sw_lf;
                                  execute immediate v_sql_cp;
                                  dbms_lock.sleep(v_sleep);
-                                 execute immediate 'alter database add logfile thread '||chloop.thread# ||' size '||v_maxbytes ;
-                                 execute immediate 'alter database drop logfile group '||chloop.group# ;
+                                 execute immediate 'alter database add logfile thread '
+                                    || chloop.thread# ||' size '|| v_maxbytes ;
+                                 execute immediate 'alter database drop logfile group '
+                                    || chloop.group#;
                            ELSE
-                                 execute immediate 'alter database add logfile thread '||chloop.thread# ||' size '||v_maxbytes ;
-                                 execute immediate 'alter database drop logfile group '||chloop.group# ;
+                                 execute immediate 'alter database add logfile thread '
+                                    ||chloop.thread# ||' size '||v_maxbytes ;
+                                 execute immediate 'alter database drop logfile group '
+                                    ||chloop.group# ;
 
                            END IF;
                        END LOOP;
@@ -263,32 +287,60 @@ def main():
                for t in (select * from v$thread where enabled != 'DISABLED')
                loop
                    dbms_output.put_line('Thread: '||t.thread#);
-                   select count(*) into v_existing_redogroups from v$log where thread# = t.thread#;
-                   dbms_output.put_line('Thread: '||t.thread# ||' numgroups: '||v_existing_redogroups);
+
+                   select count(*)
+                   into v_existing_redogroups
+                   from v$log
+                   where thread# = t.thread#;
+
+                   dbms_output.put_line('Thread: '
+                     || t.thread# || ' numgroups: ' || v_existing_redogroups);
                    IF v_existing_redogroups < v_groups THEN
-                       select v_groups-v_existing_redogroups into v_group_diff from dual;
-                       dbms_output.put_line('Adding '||v_group_diff ||' groups to thread: '||t.thread#);
+
+                       select v_groups-v_existing_redogroups
+                       into v_group_diff
+                       from dual;
+
+                       dbms_output.put_line('Adding ' || v_group_diff
+                        || ' groups to thread: ' || t.thread#);
                             for grloop in 1..v_group_diff
                             loop
                                 v_redo_group_changes := v_redo_group_changes+1;
                                 --v_max_groupnum := 0;
                                 select max(group#) into v_max_groupnum from v$log;
                                 v_max_groupnum := v_max_groupnum + 1;
-                                execute immediate 'alter database add logfile thread '|| t.thread# ||' group '||v_max_groupnum ||' size '||v_maxbytes;
-                                dbms_output.put_line('adding group '||v_max_groupnum ||' to thread '||t.thread#);
+                                execute immediate 'alter database add logfile thread '
+                                    || t.thread# || ' group ' || v_max_groupnum
+                                    ||' size ' || v_maxbytes;
+                                dbms_output.put_line('adding group ' || v_max_groupnum
+                                    || ' to thread ' || t.thread#);
                             end loop;
 
                    ELSIF v_existing_redogroups > v_groups THEN
-                       --select count(*) into v_existing_redogroups from v$log where thread# = t.thread#;
-                       select v_existing_redogroups-v_groups into v_group_diff from dual;
-                       dbms_output.put_line('Removing ' ||v_group_diff ||' groups from thread: '||t.thread#);
+                       -- select count(*)
+                       -- into v_existing_redogroups
+                       -- from v$log
+                       -- where thread# = t.thread#;
+                       select v_existing_redogroups-v_groups
+                       into v_group_diff
+                       from dual;
+                       dbms_output.put_line('Removing ' || v_group_diff
+                        || ' groups from thread: ' || t.thread#);
 
                        for grloop in 1..v_group_diff
                        loop
                            v_redo_group_changes := v_redo_group_changes+1;
-                           select max(group#) into v_max_groupnum from v$log where upper(status) not in ('ACTIVE','CURRENT') and thread# = t.thread#;
-                           execute immediate 'alter database drop logfile group '||v_max_groupnum ;
-                           dbms_output.put_line('dropping group '||v_max_groupnum ||' thread#: '||t.thread#);
+
+                           select max(group#)
+                           into v_max_groupnum
+                           from v$log
+                           where upper(status) not in ('ACTIVE','CURRENT')
+                            and thread# = t.thread#;
+
+                           execute immediate 'alter database drop logfile group '
+                                || v_max_groupnum ;
+                           dbms_output.put_line('dropping group ' || v_max_groupnum
+                                || ' thread#: ' || t.thread#);
                        end loop;
 
                    ELSE
@@ -315,12 +367,14 @@ def main():
               EXCEPTION
               WHEN missing_suffix THEN
                    dbms_output.put_line('--------');
-                   dbms_output.put_line('You need to suffix the size with (M,G or T), i.e: '||v_maxbytes ||'M/'||v_maxbytes ||'G/' ||v_maxbytes ||'T');
+                   dbms_output.put_line('You need to suffix the size with '
+                        || '(M,G or T), i.e: '
+                        || v_maxbytes || 'M/' || v_maxbytes
+                        || 'G/' || v_maxbytes || 'T');
                    dbms_output.put_line('--------');
 
        END;
     """
-
 
     cur = conn.cursor()
 
@@ -329,31 +383,36 @@ def main():
         v_group_changed = cur.var(cx_Oracle.NUMBER)
         v_size_msg = cur.var(cx_Oracle.STRING)
         v_group_msg = cur.var(cx_Oracle.STRING)
-        cur.execute(redosql,
-            {'redosize': size, 'redogroups': groups , 'o_size_changed': v_size_changed, 'o_group_changed': v_group_changed, 'o_size_msg': v_size_msg, 'o_group_msg': v_group_msg})
+        cur.execute(
+            redosql,
+            {
+                'redosize': size,
+                'redogroups': groups,
+                'o_size_changed': v_size_changed,
+                'o_group_changed': v_group_changed,
+                'o_size_msg': v_size_msg,
+                'o_group_msg': v_group_msg,
+            },
+        )
     except cx_Oracle.DatabaseError as exc:
-        error, = exc.args
+        (error,) = exc.args
         msg = '%s' % (error.message)
         module.fail_json(msg=msg, changed=False)
 
-
-
     size_result_changed = v_size_changed.getvalue()
     group_result_changed = v_group_changed.getvalue()
-    #module.exit_json(msg=result_changed, changed=False)
+    # module.exit_json(msg=result_changed, changed=False)
     size_msg = v_size_msg.getvalue()
     group_msg = v_group_msg.getvalue()
     msg = "%s. %s." % (size_msg, group_msg)
-    if (size_result_changed > 0 or group_result_changed > 0):
+    if size_result_changed > 0 or group_result_changed > 0:
         changed = True
     else:
         changed = False
-    #module.exit_json(msg=result_changed, changed=False)
-
+    # module.exit_json(msg=result_changed, changed=False)
 
     module.exit_json(msg=msg, changed=changed)
 
 
-from ansible.module_utils.basic import *
 if __name__ == '__main__':
     main()
