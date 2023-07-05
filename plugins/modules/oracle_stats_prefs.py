a86b1f9fd54fabe9868e4f9f1b951c587a0c4939
Thorsten Bruhns <thorsten.bruhns@googlemail.com>
2023-07-05 05:37:31 +0000

    oracle_stat_prefs: refactoring code, make it usable for ansible-doc, Python3 usable only

--- plugins/modules/oracle_stats_prefs.py
+++ plugins/modules/oracle_stats_prefs.py
@@ -1,10 +1,12 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+from ansible.module_utils.basic import AnsibleModule
 
 ANSIBLE_METADATA = {
     'metadata_version': '1.1',
     'status': ['preview'],
-    'supported_by': 'community'
+    'supported_by': 'community',
 }
 
 DOCUMENTATION = '''
@@ -17,49 +19,43 @@ description:
 version_added: "2.4"
 options:
     hostname:
-        description:
-            - The Oracle database host
+        description: The Oracle database host
         required: false
         default: localhost
     port:
-        description:
-            - The listener port number on the host
+        description: The listener port number on the host
         required: false
         default: 1521
     service_name:
-        description:
-            - The database service name to connect to
+        description: The database service name to connect to
         required: true
     user:
-        description:
-            - The Oracle user name to connect to the database, must have DBA privilege
+        description: >
+            The Oracle user name to connect to the database, must have DBA privilege
         required: False
     password:
-        description:
-            - The Oracle user password for 'user'
+        description: The Oracle user password for 'user'
         required: False
     mode:
-        description:
-            - The mode with which to connect to the database
+        description: The mode with which to connect to the database
         required: true
         default: normal
         choices: ['normal','sysdba']
     preference_name:
-        description:
-            - DBMS_STATS preference name
+        description: DBMS_STATS preference name
         aliases:
             - pname
     preference_value:
-        description:
-            - Preference value
+        description: Preference value
         aliases:
             - pvalue
     state:
-        description:
-            - Either to set the preference (present) or reset it to default (absent)
+        description: >
+            Either to set the preference (present) or reset it to default (absent)
         required: true
         default: present
         choices: ['present','absent']
+
 notes:
     - cx_Oracle needs to be installed
     - Oracle RDBMS 10gR2 or later required
@@ -99,27 +95,35 @@ except ImportError:
 else:
     cx_oracle_exists = True
 
+
 # Ansible code
 def main():
     global lconn, conn, msg, module
     msg = ['']
     module = AnsibleModule(
-        argument_spec = dict(
-            hostname      = dict(default='localhost'),
-            port          = dict(default=1521, type='int'),
-            service_name  = dict(required=True),
-            user          = dict(required=False),
-            password      = dict(required=False, no_log=True),
-            mode          = dict(default='normal', choices=["normal","sysdba"]),
-            preference_name = dict(required=True, aliases=['pname']),
-            preference_value = dict(aliases=['pvalue']),
-            state         = dict(default='present', choices=["present","absent"])
+        argument_spec=dict(
+            hostname=dict(default='localhost'),
+            port=dict(default=1521, type='int'),
+            service_name=dict(required=True),
+            user=dict(required=False),
+            password=dict(required=False, no_log=True),
+            mode=dict(default='normal', choices=["normal", "sysdba"]),
+            preference_name=dict(required=True, aliases=['pname']),
+            preference_value=dict(aliases=['pvalue']),
+            state=dict(default='present', choices=["present", "absent"]),
         ),
-        supports_check_mode=True
+        supports_check_mode=True,
     )
     # Check for required modules
     if not cx_oracle_exists:
-        module.fail_json(msg="The cx_Oracle module is required. 'pip install cx_Oracle' should do the trick. If cx_Oracle is installed, make sure ORACLE_HOME & LD_LIBRARY_PATH is set")
+        module.fail_json(
+            msg=(
+                "The cx_Oracle module is required. "
+                "'pip install cx_Oracle' should do the trick. "
+                "If cx_Oracle is installed, make sure ORACLE_HOME "
+                "& LD_LIBRARY_PATH is set"
+            )
+        )
     # Connect to database
     hostname = module.params["hostname"]
     port = module.params["port"]
@@ -129,7 +133,9 @@ def main():
     mode = module.params["mode"]
     wallet_connect = '/@%s' % service_name
     try:
-        if (not user and not password ): # If neither user or password is supplied, the use of an oracle wallet is assumed
+        if not user and not password:
+            # If neither user or password is supplied, the use of an
+            # oracle wallet is assumed
             if mode == 'sysdba':
                 connect = wallet_connect
                 conn = cx_Oracle.connect(wallet_connect, mode=cx_Oracle.SYSDBA)
@@ -137,22 +143,29 @@ def main():
                 connect = wallet_connect
                 conn = cx_Oracle.connect(wallet_connect)
 
-        elif (user and password ):
+        elif user and password:
             if mode == 'sysdba':
-                dsn = cx_Oracle.makedsn(host=hostname, port=port, service_name=service_name)
+                dsn = cx_Oracle.makedsn(
+                    host=hostname, port=port, service_name=service_name
+                )
                 connect = dsn
                 conn = cx_Oracle.connect(user, password, dsn, mode=cx_Oracle.SYSDBA)
             else:
-                dsn = cx_Oracle.makedsn(host=hostname, port=port, service_name=service_name)
+                dsn = cx_Oracle.makedsn(
+                    host=hostname, port=port, service_name=service_name
+                )
                 connect = dsn
                 conn = cx_Oracle.connect(user, password, dsn)
 
-        elif (not(user) or not(password)):
+        elif not (user) or not (password):
             module.fail_json(msg='Missing username or password for cx_Oracle')
 
     except cx_Oracle.DatabaseError as exc:
-        error, = exc.args
-        msg[0] = 'Could not connect to database - %s, connect descriptor: %s' % (error.message, connect)
+        (error,) = exc.args
+        msg[0] = 'Could not connect to database - %s, connect descriptor: %s' % (
+            error.message,
+            connect,
+        )
         module.fail_json(msg=msg[0], changed=False)
     if conn.version < "10.2":
         module.fail_json(msg="Database version must be 10gR2 or greater", changed=False)
@@ -163,7 +176,8 @@ def main():
     c = conn.cursor()
     var_changed = c.var(cx_Oracle.NUMBER)
     var_msg = c.var(cx_Oracle.STRING)
-    c.execute("""
+    c.execute(
+        """
     DECLARE
         v_param_name VARCHAR2(100);
         v_param_value VARCHAR2(100);
@@ -191,13 +205,19 @@ def main():
         :changed:= v_changed;
         :msg:= v_msg;
     END;
-    """, {'pname': module.params['preference_name'], 'pvalue': module.params['preference_value'], 'state': module.params['state'],
-          'changed': var_changed, 'msg': var_msg})
+    """,
+        {
+            'pname': module.params['preference_name'],
+            'pvalue': module.params['preference_value'],
+            'state': module.params['state'],
+            'changed': var_changed,
+            'msg': var_msg,
+        },
+    )
     result_changed = var_changed.getvalue() > 0
     msg[0] = var_msg.getvalue()
     module.exit_json(msg=", ".join(msg), changed=result_changed)
 
 
-from ansible.module_utils.basic import *
 if __name__ == '__main__':
     main()
